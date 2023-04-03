ddecd66d995dd09864cc54d44b515d3979cc31d1
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-04-03 17:36:54 -0300

    Realizar mitad del ejercio2

--- enunciados/src/ej1/Main.java
+++ enunciados/src/ej1/Main.java
@@ -3,6 +3,8 @@ package ej1;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		/* INICIO EJERCICIO 1 
 		for (int i=1;i<=10;i++) {
 			System.out.print(i);
 			System.out.print(" , ");
@@ -12,7 +14,12 @@ public class Main {
 			System.out.print(i);
 			System.out.print(" , ");
 		}
+		
+		FIN EJERCICIO 1 */
 
+		for(int i=0;i<10;i++) {
+			
+		}
 	}
 
 }
