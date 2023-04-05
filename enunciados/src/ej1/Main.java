99250c3dc34964d98bce8aacbafd6e490d0b7770
lkb17 <102708361+Lotar17@users.noreply.github.com>
2023-04-04 22:01:06 -0300

    Realizar ejercicio 3

--- enunciados/src/ej1/Main.java
+++ enunciados/src/ej1/Main.java
@@ -41,8 +41,31 @@ public class Main {
 		
 		// #########################################################
 		
-		
-		
+		Scanner scanner = new Scanner(System.in);
+		String[] palabras = new String[10];
+		String palabra = new String();
+		int encontro = 0;
+		for (int i =0;i<10;i++) {
+			System.out.print("ESCRIBA UNA PALABRA:\n");
+			palabras[i] = scanner.next();		
+		}
+		System.out.println();
+		System.out.print("INGRESE UNA PALABRA");
+		System.out.println();
+		palabra = scanner.next();
+		System.out.println();
+		for(int i =0;i<10;i++) {
+			if (palabras[i].equals(palabra) == true){
+				encontro = 1;
+			}
+		}
+		if (encontro == 1) {
+			System.out.print("LA PALABRA INGRESADA SE ENCUENTRA EN LA LISTA DE 10 PALABRAS");
+		}
+		else {
+			System.out.print("LA PALABRA INGRESADA NO SE ENCUENTRA EN LA LISTA DE 10 PALABRAS");
+		}
+		scanner.close();
 	}
 
 }
