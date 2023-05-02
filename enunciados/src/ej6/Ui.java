22b916290fd0f025c5b5de6b3567c49bd15a4757
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-05-02 14:55:44 -0300

    Realizar mitad ej 6

---
+++ enunciados/src/ej6/Ui.java
@@ -0,0 +1,16 @@
+package ej6;
+
+
+public class Ui {
+	
+	public static int menu() {
+		Scanner scan = new Scanner
+		System.out.println("1- ");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+}
